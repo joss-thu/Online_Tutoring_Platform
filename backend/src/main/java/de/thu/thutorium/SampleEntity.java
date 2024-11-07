3618c67f37aa96170d093e76149b22dfcd2ed5a8
Dorotej-Dado Arnold <116161408+ddarnold@users.noreply.github.com>
2024-11-07 18:37:32 +0100

    OTP-88 Migration to CloudSQL
    * Transfer to CloudSQL
    * Remove bloat

--- backend/src/main/java/de/thu/thutorium/SampleEntity.java
+++ backend/src/main/java/de/thu/thutorium/SampleEntity.java
@@ -1,6 +1,8 @@
 package de.thu.thutorium;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +16,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class SampleEntity {
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id;
     private String first_name;
     private String last_name;
--- backend/src/main/java/de/thu/thutorium/ThutoriumApplication.java
+++ backend/src/main/java/de/thu/thutorium/ThutoriumApplication.java
@@ -2,13 +2,13 @@ package de.thu.thutorium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
 
 @SpringBootApplication
 public class ThutoriumApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ThutoriumApplication.class, args);
-    }
+    public static void main(String[] args) { SpringApplication.run(ThutoriumApplication.class, args); }
 
 }
