a0f2fba89fd96bf797fd1bd4e7f010d993ba7e78
joss-thu <antojo01@thu.de>
2024-12-04 20:29:58 +0100

    Redundant code removed.

--- backend/src/main/java/de/thu/thutorium/database/dbObjects/ProgressDBO.java
+++ backend/src/main/java/de/thu/thutorium/database/dbObjects/ProgressDBO.java
@@ -3,7 +3,6 @@ package de.thu.thutorium.database.dbObjects;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
 
 /**
  * Represents the scores achieved by a student for a course.
