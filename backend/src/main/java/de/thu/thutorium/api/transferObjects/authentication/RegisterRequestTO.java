16f08ade6cf63f2f8b706f738efcff2706b8a9a3
joss-thu <antojo01@thu.de>
2024-12-09 12:18:40 +0100

    UserDBO implements UserDetails from spring security.

--- backend/src/main/java/de/thu/thutorium/api/transferObjects/authentication/RegisterRequestTO.java
+++ backend/src/main/java/de/thu/thutorium/api/transferObjects/authentication/RegisterRequestTO.java
@@ -1,10 +1,24 @@
 package de.thu.thutorium.api.transferObjects.authentication;
 
+import de.thu.thutorium.database.dbObjects.RoleDBO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-public class RegisterRequestTO {
+import java.util.Set;
 
+/**
+ * Transfer object for the user registration request.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequestTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private RoleDBO role;
 }
