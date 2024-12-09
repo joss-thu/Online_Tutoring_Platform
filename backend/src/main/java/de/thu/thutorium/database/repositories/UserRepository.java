c0114623e641892ee71d0f8061ca18cc44c12570
joss-thu <antojo01@thu.de>
2024-12-09 12:35:24 +0100

    UserRepository updated.

--- backend/src/main/java/de/thu/thutorium/database/repositories/UserRepository.java
+++ backend/src/main/java/de/thu/thutorium/database/repositories/UserRepository.java
@@ -2,7 +2,29 @@ package de.thu.thutorium.database.repositories;
 
 import de.thu.thutorium.database.dbObjects.UserDBO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+/**
+ * Repository interface for UserDBO entities.
+ * Provides methods for performing CRUD operations on user data.
+ */
+@Repository
 public interface UserRepository extends JpaRepository<UserDBO, Integer> {
+    /**
+     * Finds a user entity by its username; in this case, email.
+     *
+     * @param email the email to search for
+     * @return an Optional containing the found UserDBO, or empty if not found
+     */
+    Optional<UserDBO> findByEmail(String email);
 
+    /**
+     * Checks if a user entity exists with the given email.
+     *
+     * @param email the username to check
+     * @return true if a user entity exists with the given username, false otherwise
+     */
+    boolean existsByEmail(String email);
 }
