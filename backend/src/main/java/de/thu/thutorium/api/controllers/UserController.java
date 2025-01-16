af3f66bf53bc5820b9b8cba147c408b751f7a45d
joss-thu <antojo01@thu.de>
2025-01-16 16:47:44 +0100

    Test errors fixed in UserImplTests.

--- backend/src/main/java/de/thu/thutorium/api/controllers/UserController.java
+++ backend/src/main/java/de/thu/thutorium/api/controllers/UserController.java
@@ -19,9 +19,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
