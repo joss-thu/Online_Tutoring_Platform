e84a6890c4e73ce30fb3317811f692f790e61407
Kekschorstviy <117016566+Kekschorstviy@users.noreply.github.com>
2025-01-17 09:52:06 +0100

    Updates with Try Catch Blocks, unenroll with progress deletion

--- backend/src/main/java/de/thu/thutorium/services/interfaces/RatingCourseService.java
+++ backend/src/main/java/de/thu/thutorium/services/interfaces/RatingCourseService.java
@@ -1,6 +1,5 @@
 package de.thu.thutorium.services.interfaces;
 import de.thu.thutorium.api.transferObjects.common.RatingCourseTO;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
