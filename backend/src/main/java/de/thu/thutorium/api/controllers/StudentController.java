a863949f502c1bda1f8f5c4ee4e2d55915ac088a
Kekschorstviy <117016566+Kekschorstviy@users.noreply.github.com>
2025-01-18 15:36:37 +0100

    fix auth

--- backend/src/main/java/de/thu/thutorium/api/controllers/StudentController.java
+++ backend/src/main/java/de/thu/thutorium/api/controllers/StudentController.java
@@ -21,7 +21,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.security.sasl.AuthenticationException;
 import java.util.Collections;
 import java.util.List;
 
