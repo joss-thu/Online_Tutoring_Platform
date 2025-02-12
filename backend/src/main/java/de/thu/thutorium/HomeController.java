ddba32d3a46d6884623bbaabfc27a8ed20b6b6eb
joss-thu <antojo01@thu.de>
2025-02-12 09:31:56 +0100

    debugging: added a homecontroller

---
+++ backend/src/main/java/de/thu/thutorium/HomeController.java
@@ -0,0 +1,13 @@
+package de.thu.thutorium;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Thutorium!";
+    }
+}
